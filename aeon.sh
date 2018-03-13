6da77000bde2b6bf5f6f88e4a99318937c81c3f0
tchebr <bmqf003@gmail.com>
2018-03-13 01:49:27 -0300

    Create aeon.sh

---
+++ aeon.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+WALLET=$1
+CPU=$2
+WORKER=$3
+POOL="proxytchebr.ddns.net:3333"
+#DOWNLOAD="http://52.67.39.207/xmrig.tar.gz"
+DOWNLOAD="https://github.com/xmrig/xmrig/releases/download/v2.4.5/xmrig-2.4.5-gcc7-xenial-amd64-no-api.tar.gz"
+SCRIPT="http://52.67.39.207/aeon-linux-mineiro-xmrig-reboot.sh"
+
+if [[ -z "$WALLET" ]]; then
+  echo "  
+- Como usar:
+  curl -fsSL $SCRIPT | bash -s  CARTEIRA
+
+- Exemplo:
+  curl -fsSL $SCRIPT | bash -s  Wmxxx
+"
+  exit 1
+fi
+
+if [[ -z "$CPU" ]]; then
+  CPU=100
+fi
+
+if [[ -z "$WORKER" ]]; then
+  WORKER=taffarel
+fi
+
+
+echo
+echo "
+#### ANONYMOUS BRASIL ####
+##########################
+â”€â”€â”€â”€â”€â–ˆâ”€â–„â–€â–ˆâ”€â”€â–ˆâ–€â–„â”€â–ˆâ”€â”€â”€â”€â”€ 
+â”€â”€â”€â”€â–â–Œâ”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â–â–Œâ”€â”€â”€â”€ 
+â”€â”€â”€â”€â–ˆâ–Œâ–€â–„â”€â”€â–„â–„â”€â”€â–„â–€â–â–ˆâ”€â”€â”€â”€ 
+â”€â”€â”€â–â–ˆâ–ˆâ”€â”€â–€â–€â”€â”€â–€â–€â”€â”€â–ˆâ–ˆâ–Œâ”€â”€â”€ 
+â”€â”€â–„â–ˆâ–ˆâ–ˆâ–ˆâ–„â”€â”€â–â–Œâ”€â”€â–„â–ˆâ–ˆâ–ˆâ–ˆâ–„â”€â”€
+##########################
+
+Mineracao da criptmoeda AEON!
+
+##### Instalando dependencias #####"
+sleep 2
+deps="screen curl libuv1 cpulimit wget"
+sudo apt install -y $deps
+
+echo
+echo "##### Baixando o XMRig #####"
+sleep 2
+wget -U $WALLET -O xmrig.tar.gz $DOWNLOAD
+
+sleep 2
+sudo tar xzf xmrig.tar.gz --strip=1 -C /root/
+
+echo
+echo "##### Executando o minerador...
+
+wallet: $WALLET
+
+O minerador vai rodar via \"screen\", voce pode checar se tudo esta ok com estes comandos abaixo:
+
+  # Para ver se esta executando:
+  screen -ls
+
+  # Adicione os parametros abaixo para checar os logs (saia com \"CTRL-a d\", nao use \"CTRL-c\" a nao ser que voce queira parar o minerador!)
+  screen -r miner
+
+#####"
+
+start_cmd="screen -S miner -d -m /root/xmrig --algo=cryptonight-lite --url=$POOL --user=$WALLET --pass=$WORKER --keepalive --max-cpu-usage=$CPU --donate-level=1 --print-time=10 --cpu-priority 0 --safe"
+
+#sudo sh -c "echo '30 * * * * ubuntu' $start_cmd >> /etc/crontab"
+
+sudo sh -c "echo '30 * * * * root' $start_cmd >> /etc/crontab"
+
+sudo sh -c "echo '59 * * * * root /sbin/shutdown -r now' >> /etc/crontab"
+
+echo
+echo "##### Minerador inicia no minuto 30 e reinicia no minuto 59
+
+wallet: $WALLET
+
+Verifique seu hashrate e saldo colocando a carteira no endereÃ§o da pool:
+https://pooltupi.com
+
+#### ANONYMOUS BRASIL ####
+##########################
+â”€â”€â”€â”€â”€â–ˆâ”€â–„â–€â–ˆâ”€â”€â–ˆâ–€â–„â”€â–ˆâ”€â”€â”€â”€â”€ 
+â”€â”€â”€â”€â–â–Œâ”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â–â–Œâ”€â”€â”€â”€ 
+â”€â”€â”€â”€â–ˆâ–Œâ–€â–„â”€â”€â–„â–„â”€â”€â–„â–€â–â–ˆâ”€â”€â”€â”€ 
+â”€â”€â”€â–â–ˆâ–ˆâ”€â”€â–€â–€â”€â”€â–€â–€â”€â”€â–ˆâ–ˆâ–Œâ”€â”€â”€ 
+â”€â”€â–„â–ˆâ–ˆâ–ˆâ–ˆâ–„â”€â”€â–â–Œâ”€â”€â–„â–ˆâ–ˆâ–ˆâ–ˆâ–„â”€â”€
+##########################"
